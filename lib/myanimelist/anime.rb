8b99c7d8fb75328b7c24b82ba5ef545fbca5522e
FormalHellhound <FormalHellhound@gmail.com>
2016-12-05 15:45:07 -0500

    bdvhdhjdgjd

--- lib/myanimelist/anime.rb
+++ lib/myanimelist/anime.rb
@@ -8,11 +8,11 @@ module MyAnimeList
 
     def search(name)
       query = name.split(' ')
-      name = ''
-      query.each_with_index do |query, index|
-        name += "q=#{query}"
-        name += "&" unless (index == query.size - 1)
-      end
+      name = 'q=FMA&q=Brotherhood'
+      #query.each_with_index do |query, index|
+        #name += "q=#{query}"
+        #name += "&" unless (index == query.size - 1)
+      #end
       puts name
       get_search(name)
     end
