a6cff820c5b5e8ef689a7300ea20a250a0bfaa70
FormalHellhound <FormalHellhound@gmail.com>
2016-12-05 15:46:35 -0500

    ggertegt

--- lib/myanimelist/anime.rb
+++ lib/myanimelist/anime.rb
@@ -20,7 +20,7 @@ module MyAnimeList
     def get_search(name)
       response = RestClient::Request.new(
         method: :get,
-        url: "https://myanimelist.net/api/anime/search.xml?#{CGI::escape name}",
+        url: "https://myanimelist.net/api/anime/search.xml?#{name}",
         user: @myanimelist_username,
         password: @myanimelist_password,
         content_type: :xml ).execute
