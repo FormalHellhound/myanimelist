6b54a611724ac922289451cbc19b9116b750454c
FormalHellhound <FormalHellhound@gmail.com>
2016-12-06 15:27:14 -0500

    asfasf

--- lib/myanimelist/anime.rb
+++ lib/myanimelist/anime.rb
@@ -8,7 +8,7 @@ module MyAnimeList
 
     def search(name)
       #query = name.split(' ')
-      name = "q=#{CGI::escape 'FMA+Shamballa'}"#{CGI::escape 'Shamballa'}"
+      #name = "q=#{CGI::escape 'FMA+Shamballa'}"#{CGI::escape 'Shamballa'}"
       #name = 'q='
       #escape_symbol = '+'
       #query.each_with_index do |q, index|
@@ -20,7 +20,7 @@ module MyAnimeList
     end
 
     def get_search(name)
-      response = IO.popen("curl -G -u #{@myanimelist_username}:#{@myanimelist_password} http://myanimelist.net/api/anime/search.xml --data-urlencode #{'q=FMA Shamballa'}")
+      response = IO.popen("curl -u #{@myanimelist_username}:#{@myanimelist_password} https://myanimelist.net/api/anime/search.xml?q=#{name}")
       parse_xml response
     end
 
