b7141f0d47253ecb912f33362556f0051301573d
FormalHellhound <FormalHellhound@gmail.com>
2016-12-06 13:26:00 -0500

    sdfsdg

--- lib/myanimelist/anime.rb
+++ lib/myanimelist/anime.rb
@@ -7,13 +7,13 @@ module MyAnimeList
     end
 
     def search(name)
-      query = name.split(' ')
+      #query = name.split(' ')
       #name = "q=#{CGI::escape 'FMA'}&q=#{CGI::escape 'Shamballa'}"
-      name = ''
-      query.each_with_index do |q, index|
-        name += "#{CGI::escape q}"
-        name += '%2B' unless (index+1 == query.length)
-      end
+      #name = ''
+      #query.each_with_index do |q, index|
+        #name += "#{CGI::escape q}"
+        #name += '%2B' unless (index+1 == query.length)
+      #end
       puts name
       get_search(name)
     end
@@ -21,16 +21,15 @@ module MyAnimeList
     def get_search(name)
       response = RestClient::Request.execute(
         method: :get,
-        url: "https://myanimelist.net/search/prefix.json?type=anime&keyword=#{name}",
+        url: "https://myanimelist.net/api/anime/search.xml?q=#{CGI::escape name}",
         user: @myanimelist_username,
         password: @myanimelist_password,
-        content_type: :json)
-      parse_json response
+        content_type: :xml)
+      parse_xml response
     end
 
-    def parse_json(response)
-      anime = JSON.parse(response)
-      #serialize response
+    def parse_xml(response)
+      serialize Hash.from_xml response
     end
 
     def serialize(data)
